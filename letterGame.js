f726f6b8e12478068e400bbb6a6e703657af6f5d
konghouyin <1360234119@qq.com>
2018-09-23 23:39:45 +0800

    打字游戏
    已完成文字动画移动，和血量适配。

--- letterGame.js
+++ letterGame.js
@@ -5,19 +5,22 @@
         pageMax: 4,//说明书页数
         pageNow: 1,//说明书当前页数
         num: 5,//单词个数
-        live:100,//生命总值
+        live: 100,//生命总值
+        liveMax:100,
         continueMax:5,//连击倍率最大值
         continueEach: 0.35,//连击递增倍率
         continueSub:0.05,//连击时间递减
         timeStart:0,
         timePause: 0,
-        green:250,//绿色退后触发几率
+        green: 250,//绿色退后触发几率
+        line: 80,
+        flag:0,//控制游戏起止
     }
     var data = {
         plans: document.getElementsByClassName('target')[0],
         id:1//工厂id
     }
-    var letterAll = [];
+    var letterAll = new Array();
 
     //说明书按钮
     function pageCtrl() {
@@ -95,7 +98,6 @@
         det = timeNow - obj.timeStart;
         second = (det / 1000);
         minute = parseInt(second / 60);
-        console.log(minute);
         second = parseInt(second - minute * 60);
         timeShow.innerHTML = timeTurn(minute) + ":" + timeTurn(second);
     }
@@ -103,31 +105,43 @@
     //游戏逻辑
     function gameStart() {
         antoMove();
+        obj.flag = 1;
         setTimeout(function () {
             obj.timeStart = new Date();
             setInterval(timeGo, 174);
             creatLetter();
+            var timer = setInterval(move, 15);
         }, 3000)
-        
-
-
     }
 
+    //分类型创建
+    function creatLetter() {
+        if (obj.flag == 1) {
+            var num = data.plans.children[0].children.length;
+            if (num >= obj.num) {
+                setTimeout(creatOneLetter, 1000);
+            } else {
+                var time = Math.floor(1500 / (obj.num - num));
+                setTimeout(creatOneLetter, time);
+            }
+        }
+    }
 
-
+    //创建具体过程
     function creatOneLetter(){
-        var let = factoryLetter();
-        console.log(let);
+        var let = new factoryLetter();
         let.letter.innerHTML = String.fromCharCode(let.thing);
         if (let.style == 0) {
             let.letter.setAttribute("class", "green");
+            let.creatTime = new Date();
         } else {
             let.letter.setAttribute("class", "white");
         }
-        let.letter.style.top = let.positionX + "px";
-        let.letter.style.left = let.positionY + "px";
+        let.letter.style.top = let.positionY + "%";
+        let.letter.style.left = let.positionX + "%";
         data.id++;
         letterAll.push(let);
+        creatLetter();
     }
 
     //字母工厂
@@ -135,15 +149,57 @@
         this.letter = document.createElement('li');
         this.letter.setAttribute('id', "id"+data.id );
         this.thing = Math.floor(Math.random() * 26 + 65);
-        this.positionX = 50;
-        this.positionY = 500 * Math.random();
+        this.randomX = Math.random();
+        this.randomY = this.randomX;
+        this.positionY = 7;
+        this.positionX = 95 * Math.random();
         this.style = Math.floor(Math.random()*obj.green);
         data.plans.children[0].appendChild(this.letter);
-        return this;
+    }
+    //减血函数
+    function lessLive() {
+        var live = document.getElementsByClassName('main')[0];
+        obj.live -= 0.01;
+        live.style.width = obj.live / 100 * 180 + "px";
+        if (obj.live <= 0) {
+            obj.flag = 0;
+            //游戏结束
+        }
     }
 
-    main();
-    function main() {
+    //移动
+    function move() {
+        var real = letterAll.length;
+        while (real--) {
+            var really = letterAll[real];
+            really.positionY += really.randomY * 0.05+0.05;
+            really.positionX += really.randomX * 0.15;
+            if (really.positionX >= 95) {
+                really.randomX = -really.randomX;
+            }
+            if (really.positionX <= 5) {
+                really.randomX = -really.randomX;
+            }
+            if (really.positionY > obj.line-5) {
+                really.positionY = 7;
+                really.letter.classList.remove("white");
+                really.letter.classList.add("red");
+                really.style = -1;
+                lessLive();
+            }
+            really.letter.style.top = (really.positionY) + "%";
+            really.letter.style.left = (really.positionX) + "%";
+            if (really.style == 0) {
+                //控制class改变
+            }
+            if (really.style == -1) {
+                lessLive();
+            }
+        }
+    }
+
+    init();
+    function init() {
         pageCtrl();
     }
 
